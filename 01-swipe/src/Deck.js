3798bdc4796a7c5b4810e678ada87dd63a046cc0
Ramon Boza <bozanayade@gmail.com>
2019-02-21 12:29:00 +0100

    Setting threshold to whether the user likes the card or not

--- 01-swipe/src/Deck.js
+++ 01-swipe/src/Deck.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = SCREEN_WIDTH * 0.5;
 
 class Deck extends Component {
 
@@ -20,8 +21,14 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({ x: gesture.dx, y: gesture.dy });
             },
-            onPanResponderRelease: () => {
-                this.resetPosition();
+            onPanResponderRelease: (event, gesture) => {
+                if( gesture.dx > SWIPE_THRESHOLD ) {
+                    console.log('swipe right!')
+                } else if (gesture.dx < - SWIPE_THRESHOLD ) {
+                    console.log('swipe left!')
+                } else {
+                    this.resetPosition();
+                }
             }
         });
 
